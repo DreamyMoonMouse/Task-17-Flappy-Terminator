eaabebbc372d3e72261ee86f37971b43971d9316
DMouse <noname666666666@ya.ru>
2025-11-26 11:52:17 +0300

    Fix: refactor

--- Assets/Scripts/Camera/PlaneTracker.cs
+++ Assets/Scripts/Camera/PlaneTracker.cs
@@ -5,7 +5,7 @@ public class PlaneTracker : MonoBehaviour
    [SerializeField] private float _xOffset;
    [SerializeField] private Plane _plane;
    
-   private void Update()
+   private void LateUpdate()
    {
        var position = transform.position;
        position.x = _plane.transform.position.x + _xOffset;
